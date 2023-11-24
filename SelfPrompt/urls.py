5d8dbfa5e7abfd30d8181d7d19af03dd57e63d80
Delroy Brown <delroybrown.db@gmail.com>
2023-11-24 19:10:54 +0000

    Added media queries for mobile

--- SelfPrompt/urls.py
+++ SelfPrompt/urls.py
@@ -6,7 +6,7 @@ from .views import home, render_form, save_image
 urlpatterns = [
     path('', render_form, name='render_form'),  # For handling GET requests to the homepage
     path('submit/', home, name='home'),  # For handling POST requests
-     path('save_image/', save_image, name='save_image'),
+    path('save_image/', save_image, name='save_image'),
 ]
 
 if settings.DEBUG:
